bee8867d33a71766b91c479d3c4046cad375ba1b
Mateusz <mateuszgalwas@gmail.com>
2025-11-23 21:05:52 +0100

    detale produktu

--- myCoolApp/src/app/product-details/product-details.component.ts
+++ myCoolApp/src/app/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NativeScriptCommonModule } from "@nativescript/angular";
+import { ActivatedRoute } from "@angular/router";
+import { ProductService } from "../products/product.service";
 
 @Component({
     selector: "ProductDetails",
@@ -7,4 +9,26 @@ import { NativeScriptCommonModule } from "@nativescript/angular";
     imports: [NativeScriptCommonModule],
     templateUrl: "./product-details.component.html",
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent implements OnInit {
+
+    product: any = null;
+    loading = true;
+    error: string | null = null;
+
+    constructor(
+        private route: ActivatedRoute,
+        private productService: ProductService
+    ) {}
+
+    async ngOnInit() {
+        const id = Number(this.route.snapshot.params["id"]);
+
+        try {
+            this.product = await this.productService.getProductById(id);
+        } catch (e) {
+            this.error = "Nie udało się pobrać szczegółów produktu.";
+        } finally {
+            this.loading = false;
+        }
+    }
+}
